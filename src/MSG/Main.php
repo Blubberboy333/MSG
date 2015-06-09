4e0b70b7043aeb77c746badd99097a5b698bd3fe
Blubberboy333 <thecowmooman@gmail.com>
2015-06-08 21:01:26 -0400

    Create Main.php

---
+++ src/MSG/Main.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Main;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase{
+	public function onEnable(){
+		$this->getLogger()->info("Enabled");
+	}
+	public function onDisable(){
+		
+	}
+	
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+		switch($command->getName()){
+			case "message":
+			case "msg": 
+			if(count($args) < 2){
+				$sender->sendMessage(TextFormat::RED . "Usage: " . TextFormat::RESET . "/msg <player> [message]");
+				return true;
+			}else{
+				$name = strtolower(array_shift($args));
+				$player = $sender->getServer()->getPlayer($name);
+				
+				if($player instanceof Player){
+					$sender->sendMessage("[" .TextFormat::ORANGE . "me" . TextFormat::PURPLE . "->" . TextFormat::ORANGE . $player->getName(). TextFormat::RESET . "] " .implode(" ", $args));
+					$player->sendMessage("[" .TextFormat::ORANGE .$sender-.getName(). TextFormat::PURPLE . "->" . TextFormat::ORANGE. "me" . TextFormat::RESET . "] " .implode(" ", $args));
+					$this->getLogger()->info("[" . TextFormatORANGE . $sender->getName() . TextFormat::PURPLE . "->" . TextFormat::ORANGE . $player->getName() . TextFormat::RESET . "] " implode(" ", $args));
+					return true;
+				}else{
+					$sender->sendMessage(TextFormat::RED . "Error: " TextFormat::RESET . "That player isn't online!");
+					return true;
+				}
+			}
+		}
+	}
+}
